5c6811841c9fe95ce4621d84ee66841115d3fef2
Dương Vũ Thành Long <infz3r0@gmail.com>
2017-12-20 20:05:28 +0700

    .

--- QuanLySinhVien/UI/Properties/Resources.Designer.cs
+++ QuanLySinhVien/UI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DXApplication1.Properties {
+namespace UI_Tier.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DXApplication1.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DXApplication1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UI_Tier.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
